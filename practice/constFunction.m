66ecfbd1598fedde9e6eeb9eb4280130de6ec41b
gereziherw <gereziherw@gmail.com>
2015-07-23 16:07:30 +0300

    Machine Learning Exercise Solutions
    Stanford Machine Learning Exercise Solutions

---
+++ practice/constFunction.m
@@ -0,0 +1,10 @@
+function [ jval,gradient ] = constFunction( theta )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+jval=(theta(1)-5)^2+(theta(2)-5)^2;
+gradient=zeros(2,1);
+gradient(1)=2*(theta(1)-5);
+gradient(2)=2*(theta(2)-5);
+
+end
+
